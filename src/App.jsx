b1126fc72c630f7d1da81403ec352f950cb3deff
Hadi Bagheri <Hadi.b.1998@gmail.com>
2024-12-13 17:30:29 +0330

    update code to test fetch by query

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import './App.css'
 
 function App() {
+  async function getData(userName) {
+    const response = await fetch(`https://my-json-server.typicode.com/hadi-b-js/Easy-Todo/users/${userName}`)
+    const data = await response.json()
+    console.log(data)
+  }
+
+  getData("hadi.b.js")
 
   return (
     <>
-
+      
     </>
   )
 }
